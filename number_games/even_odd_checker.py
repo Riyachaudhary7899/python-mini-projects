e5bd94f05558aea6ebee69a79c80e501baa21e97
Riya <riyadengree189251@gmail.com>
2026-02-01 20:39:00 +0530

    Create even_odd_checker.py

---
+++ number_games/even_odd_checker.py
@@ -0,0 +1,9 @@
+# Even or Odd Checker
+# This program checks whether a number is even or odd
+
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    print(number, "is an Even number")
+else:
+    print(number, "is an Odd number")
